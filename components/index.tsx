739b6ea7f1571357d2017f27341eda9dc72da388
Darma <pedrodarma@gmail.com>
2023-09-26 20:52:18 -0300

    Remove unused components

--- components/index.tsx
+++ components/index.tsx
@@ -1,3 +1,2 @@
 export * from "./app_design";
-export * from "./workflow";
 export * from "./workflow_graphic";
--- components/workflow/index.tsx
+++
@@ -1,4 +0,0 @@
-export * from "./init-view";
-export * from "./end-view";
-export * from "./conditional-view";
-export * from "./action-view";
