80599300437c062ba8acf8ec15f45c75df533302
Darma <pedrodarma@gmail.com>
2023-09-27 15:15:11 -0300

    Update to handle errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import AppProvider from "@/src";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
         <AppProvider>
           {children}
         </AppProvider>
+        <Toaster position="bottom-left" />
       </body>
     </html>
   );
